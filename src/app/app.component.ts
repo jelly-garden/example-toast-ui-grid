6fd9e3a1f9a4eaf360bf311cbe097544eb188e95
장정원 <sehee@innodep.com>
2021-01-06 14:15:21 +0900

    테마 설정

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Grid from 'tui-grid';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'example-toast-ui';
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+    // Grid.applyTheme('default');
+    Grid.applyTheme('striped');
+    // Grid.applyTheme('clean');
+
+    /* 테마 커스터마이징 */
+    // Grid.applyTheme('striped', {
+    //   cell: {
+    //     header: {
+    //       background: '#eef'
+    //     },
+    //     evenRow: {
+    //       background: '#fee'
+    //     }
+    //   }
+    // });
+    // Grid.applyTheme('default', {
+    //   cell: {
+    //     normal: {
+    //       background: '#fff',
+    //       border: '#e0e0e0',
+    //       showVerticalBorder: false,
+    //       showHorizontalBorder: true
+    //     },
+    //     header: {
+    //       background: '#eef',
+    //       border: '#e0e0e0'
+    //     },
+    //     selectedHeader: {
+    //       background: '#e0e0e0'
+    //     }
+    //   }
+    // });
+  }
 }
